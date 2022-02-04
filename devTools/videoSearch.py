fa8a2328e796da42aa984dc4be283ef4244f4069
Tr4shL0rd <ketil04@gmail.com>
2022-02-04 14:11:41 +0100

    added script to get song url based on searchTerm

---
+++ devTools/videoSearch.py
@@ -0,0 +1,28 @@
+from youtubesearchpython import VideosSearch
+import argparse
+parser = argparse.ArgumentParser(description='Search for a video on youtube')
+parser.add_argument('--search', "-s", metavar='search', type=str, help='the search term')
+parser.add_argument("--verbose", "-v", action="store_true", help="verbose output")
+args = parser.parse_args()
+
+def searchVideo(search, verbose=False):
+    hits = {}
+    videosSearch = VideosSearch(search, limit = 1)
+    results = videosSearch.result()
+    hits["Link"] = results["result"][0]["link"] 
+    hits["Song Title"] = results["result"][0]["title"]
+    hits["Channel"] = results["result"][0]["channel"]["name"].lower().replace(" - topic", "")
+    if verbose:
+        for k,v in hits.items():
+            print(f"{k}: {v}")
+        return hits
+    else:
+        return hits["link"]
+
+if args.search:
+    result = searchVideo(args.search, verbose=args.verbose)
+else:
+    result = searchVideo(input("Search: "), verbose=args.verbose)
+    
+with open("devTools/urls.txt", "w") as f:
+    f.write(result["Link"])
