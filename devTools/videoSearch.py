3031811d449c1319c1477acc45e85cbe293f3613
Tr4shL0rd <ketil04@gmail.com>
2022-02-11 11:27:25 +0100

    made song titles better

--- devTools/videoSearch.py
+++ devTools/videoSearch.py
@@ -1,16 +1,18 @@
 from youtubesearchpython import VideosSearch
 import argparse
+import helper
 parser = argparse.ArgumentParser(description='Search for a video on youtube')
 parser.add_argument('--search', "-s", metavar='search', type=str, help='the search term')
 parser.add_argument("--verbose", "-v", action="store_true", help="verbose output")
 args = parser.parse_args()
 
+hits = {}
 def searchVideo(search, verbose=False):
-    hits = {}
     videosSearch = VideosSearch(search, limit = 1)
     results = videosSearch.result()
     hits["Link"] = results["result"][0]["link"] 
     hits["Song Title"] = results["result"][0]["title"]
+    #hits["Song Title"] = helper.fixSongNames(results["result"][0]["title"])
     hits["Channel"] = results["result"][0]["channel"]["name"].lower().replace(" - topic", "")
     if verbose:
         for k,v in hits.items():
@@ -23,6 +25,11 @@ if args.search:
     result = searchVideo(args.search, verbose=args.verbose)
 else:
     result = searchVideo(input("Search: "), verbose=args.verbose)
-    
+
 with open("devTools/urls.txt", "a") as f:
     f.writelines(f"{result}\n")
+    print(f"added   {hits['Song Title']} to urls.txt\n")
+    print(f'Title   {hits["Song Title"]}')
+    print(f'Channel {hits["Channel"]}')
+    print(f'Link    {hits["Link"]}')
+
